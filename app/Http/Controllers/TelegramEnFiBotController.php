1b634eeea8c14ee073fd60ff6aa84e6fc480ad98
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2022-09-20 10:53:46 +0300

    telegram bot4

--- app/Http/Controllers/TelegramEnFiBotController.php
+++ app/Http/Controllers/TelegramEnFiBotController.php
@@ -15,32 +15,14 @@ class TelegramEnFiBotController extends BaseController
     private $token = '5773821899:AAGhBn9Vx4EDlOlsO_O4ceTncFYp0KCbUW8';
 
     private $id;
-    private $name;
-    private $username;
-    private $lang;
-
-    private $text;
-    private $menu;
     private $message;
-    private $command;
 
     public function index(Request $request, $token){
 
         $this->message = json_encode($request->all());
         $this->id = $request->input('message.from.id');
-        $this->name = $request->input('message.from.first_name');
-        $this->username = $request->input('message.from.username');
-        $this->lang = $request->input('message.from.language_code');
 
-        $commandPos = strpos($request->input('message.text'), " "); 
-        $this->command = $commandPos>0 ? strstr($request->input('message.text'), ' ', true) : $request->input('message.text');
-
-        $this->text = $this->message;
-
-        $keyboard = array(array("/word","/repeat","/restart"));
-        $this->menu = array("keyboard" => $keyboard,"resize_keyboard" => true,"one_time_keyboard" => true);
-
-        return $this->TelegramApi('sendMessage',$this->id, ['text' => ($this->text), 'reply_markup' => json_encode($this->menu)]);   
+        return $this->TelegramApi('sendMessage', $this->id, ['text' => ($this->message)]);
     }
 
     private function TelegramApi($method,$id,$param) {
