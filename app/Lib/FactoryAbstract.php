d29d7ec26c11b4a88450425d44085cbe5926ffb8
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2022-11-01 13:07:38 +0200

    test

--- app/Lib/FactoryAbstract.php
+++ app/Lib/FactoryAbstract.php
@@ -5,14 +5,22 @@ namespace App\Lib;
 abstract class FactoryAbstract {
      
      public function create($type) {
-
-        $product = 'Product'.ucwords($type);
-        if($product) {
+/*
+        $product = "Product".ucwords($type);
+        if(class_exists($product))
+        {
           return new $product();
-        } else {
-            echo class_exists($product);
-            echo $product;
+        }
+        else {
           return new ProductB();
         }
+*/
+          switch ($type) {
+              case'A':
+                  return new ProductA();
+              case'B':
+              default:
+                  return new ProductB();
+          }
      }
  }
