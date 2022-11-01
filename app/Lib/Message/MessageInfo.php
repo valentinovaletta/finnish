d035078ac76c1ac1159b2a4bc76c43eefe383507
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2022-11-01 15:14:35 +0200

    Message Factory

---
+++ app/Lib/Message/MessageInfo.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Lib\Message;
+
+class MessageInfo extends Message{
+    private $text = "Hello!\r\nThis is an info message!";
+    private $menu = array("keyboard" => array(array("/start","/info")),"resize_keyboard" => true,"one_time_keyboard" => true);
+
+    public function getText(){
+        return $this->text;
+    }
+    public function getMenu(){
+        return $this->menu;
+    }    
+}
