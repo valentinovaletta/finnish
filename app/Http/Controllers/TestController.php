b24b1ec5eb2c4a9d13027627e421ff857617eded
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2022-11-01 12:46:28 +0200

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
 use App\Lib\ProductFactory;
-use App\MyApiClient; // import using namespace
+use App\Lib\MyApiClient; // import using namespace
 
 class TestController extends BaseController
 {
@@ -13,12 +13,7 @@ class TestController extends BaseController
         //$product = ProductFactory::build('test', 1, 'test');
         //return $product->getType();
         $foo = new MyApiClient;
-        return $this->test($foo);
-    }
-
-    public function test()
-    {
-        return 1;
+        return $foo;
     }
 }
 
--- app/MyApiClient.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App; // <- important
-
-class MyApiClient
-{
-  // your code
-}
