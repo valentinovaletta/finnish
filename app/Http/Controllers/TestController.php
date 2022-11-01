0c7600a5e8d3169505e5ba26512683191485da0e
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2022-11-01 12:40:50 +0200

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
 use App\Lib\ProductFactory;
-use App\MyApiClient; // import using namespace
+use App\Lib\MyApiClient; // import using namespace
 
 class TestController extends BaseController
 {
