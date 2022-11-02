17592a6430dc41c17fe50e49f5c8c466f9bf42dd
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2022-11-02 11:03:15 +0200

    create user

--- app/Lib/Message/Messages/MessageStart.php
+++ app/Lib/Message/Messages/MessageStart.php
@@ -2,10 +2,14 @@
 
 namespace App\Lib\Message\Messages;
 
+use App\Models\User;
+
 class MessageStart extends Message{
 
     private $id;
     private $param;
+
+    private $user;
 
     private $text;
     private $menu;
@@ -14,7 +18,9 @@ class MessageStart extends Message{
         $this->id = $id;
         $this->param = $param;
 
-        $this->text = "Hello!\r\nThis is a Start message!\r\nYour id is $this->id\r\nYour name is ".$this->param['name']."\r\nYour lang is ".$this->param['lang'];
+        $this->user = User::firstOrCreate(['id' => $this->id, 'name' => $this->param['name'], 'language' => $this->param['lang']]);
+
+        $this->text = "Hello!\r\nThis is a Start message!\r\nYour id is $this->id\r\nYour name is ".$this->param['name']."\r\nYour lang is ".$this->param['lang']."\r\nUser object is ".print_r($this->user, true);
         $this->menu = array("keyboard" => array(array("/start","/info")),"resize_keyboard" => true,"one_time_keyboard" => true);
     }
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'id',
         'name',
+        'language',
+        'points',
         'email',
         'password',
     ];
