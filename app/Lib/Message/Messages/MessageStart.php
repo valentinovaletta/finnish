dad05c39bd06def0e100021a682ffa70cc360f50
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2022-11-02 11:32:21 +0200

    message start

--- app/Lib/Message/Messages/MessageStart.php
+++ app/Lib/Message/Messages/MessageStart.php
@@ -18,9 +18,23 @@ class MessageStart extends Message{
         $this->id = $id;
         $this->param = $param;
 
-        $this->user = User::firstOrCreate(['id' => $this->id, 'name' => $this->param['name'], 'language' => $this->param['lang']]);
+        $newUser = $this->newUser();
+        $this->setText($newUser);
+    }
 
-        $this->text = "Hello!\r\nThis is a Start message!\r\nYour id is $this->id\r\nYour name is ".$this->param['name']."\r\nYour lang is ".$this->param['lang']."\r\nUser object is ".print_r($this->user, true);
+    private function newUser(){
+        $this->user = User::firstOrCreate(['id' => $this->id, 'name' => $this->param['name'], 'language' => $this->param['lang']]);
+        return $this->user->wasRecentlyCreated;
+    }
+    private function setText($newUser){
+
+        if($newUser){
+            $greeting = "Hello, ".$this->param['name']."! You are a new user here!";
+        } else {
+            $greeting = "Hello again, ".$this->param['name']."!";
+        }
+
+        $this->text = "$greeting\r\nThis is a Start message!";
         $this->menu = array("keyboard" => array(array("/start","/info")),"resize_keyboard" => true,"one_time_keyboard" => true);
     }
 
