e39e75d3b4584f6082ea899e7424590e0b4a1406
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2022-11-04 17:33:46 +0200

    newWord

--- app/Lib/Newwords/CambridgeParserLibrary.php
+++ app/Lib/Newwords/CambridgeParserLibrary.php
@@ -71,13 +71,15 @@ class CambridgeParserLibrary {
     private function GetImgUnsplashApi($word){
         $UnsplashResponce = $this->CallDictionaryApi(urlencode("https://api.unsplash.com/search/photos/?client_id=3d5fKAxk_gmo9I8XI20kCQWf0j0r1foLd6E7kuLaq0k&page=1&per_page=1&query=$word"));
 
-        if( array_key_exists('results', $UnsplashResponce) && $UnsplashResponce['total'] > 0 ){
-            $img = $UnsplashResponce['results'][0]['urls']['small'];
-        } else {
-            $img = false;
-        }
+        print_r( $UnsplashResponce );
 
-        return $img;
+        // if( array_key_exists('results', $UnsplashResponce) && $UnsplashResponce['total'] > 0 ){
+        //     $img = $UnsplashResponce['results'][0]['urls']['small'];
+        // } else {
+        //     $img = false;
+        // }
+
+        // return $img;
     }
 
     private function CallDictionaryApi($url){
