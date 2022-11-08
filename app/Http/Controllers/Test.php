5345a0db09e271fe39ffac7f054ba80d399c5450
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2022-11-08 12:35:46 +0200

    test db

--- app/Http/Controllers/Test.php
+++ app/Http/Controllers/Test.php
@@ -2,19 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
-
 class Test extends Controller{
 
     public function index(){
-
-        $wordsSet = [];
-        for($i=301; $i <= 400; $i++){
-            $wordsSet[$i] = ['tag_id' => 4, 'word_id' => $i];
-        }
-
-        print_r( $wordsSet );
-        DB::table('tag_words')->insert($wordsSet);
 
     }
 
