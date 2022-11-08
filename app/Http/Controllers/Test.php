048729a586174ae8f1414b7b37fbd9156bf07ed2
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2022-11-08 12:33:11 +0200

    test db

--- app/Http/Controllers/Test.php
+++ app/Http/Controllers/Test.php
@@ -9,8 +9,8 @@ class Test extends Controller{
     public function index(){
 
         $wordsSet = [];
-        for($i=201; $i <= 300; $i++){
-            $wordsSet[$i] = ['tag_id' => 3, 'word_id' => $i];
+        for($i=301; $i <= 400; $i++){
+            $wordsSet[$i] = ['tag_id' => 4, 'word_id' => $i];
         }
 
         print_r( $wordsSet );
