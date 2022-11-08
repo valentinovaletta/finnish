a5ca347cbf60a0becdfb17dcbcc5352525e91a8f
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2022-11-08 15:52:47 +0200

    get words

--- app/Http/Controllers/Test.php
+++ app/Http/Controllers/Test.php
@@ -15,6 +15,7 @@ class Test extends Controller{
         ->join('en_dictionaries', $this->id.'_vocabulary.word_id', '=', 'en_dictionaries.id')
         ->join('fi_dictionaries', $this->id.'_vocabulary.word_id', '=', 'fi_dictionaries.id')
         ->select($this->id.'_vocabulary.word_id', $this->id.'_vocabulary.points', 'en_dictionaries.word as enword', 'en_dictionaries.pos as pos', 'en_dictionaries.ts as ts', 'en_dictionaries.img as img', 'fi_dictionaries.word as fiword')
+        ->take(4)
         ->get()->toArray();
 
         var_dump($data);
