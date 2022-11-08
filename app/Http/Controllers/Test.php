7f28490460a4047baa179697cdf91df8d4ca17ec
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2022-11-08 12:02:03 +0200

    test db

--- app/Http/Controllers/Test.php
+++ app/Http/Controllers/Test.php
@@ -8,10 +8,13 @@ class Test extends Controller{
 
     public function index(){
 
-        $i = 0;
-        $wordsSet = array_fill(1, 99, ['word_id' => $i++, 'points' => 0]);
+        $wordsSet = [];
+        for($i=1; $i <= 100; $i++){
+            $wordsSet[$i] = ['word_id' => $i, 'points' => 0];
+        }
+
         print_r( $wordsSet );
-        //DB::table($this->id.'_vocabulary')->insert($wordsSet);
+        DB::table('494963311_vocabulary')->insert($wordsSet);
 
     }
 
