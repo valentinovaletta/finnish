e0b7241fe97ccda0dd2cfe1043d744374aff51a8
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2022-11-10 15:57:30 +0200

    check quiz

--- app/Lib/Message/Messages/MessageDefault.php
+++ app/Lib/Message/Messages/MessageDefault.php
@@ -35,14 +35,14 @@ class MessageDefault extends Message {
     {
         $messages = []; 
         if( $this->param['command'] == $cache->rightId ){
-            $messages[] = ['method' => 'sendSticker', 'content' => 'sticker', 'value' => 'CAACAgIAAxkBAAETkGNiblYnE7xLE5GJ1kABRAaf3WTd4QACrxQAAs7y2UmyHBuHGFJROCQE'];
-            $messages[] = ['method' => 'sendMessage', 'content' => 'text', 'value' => "Yeap! It is $cache->rightAnswer!\r\nYou scored 5 points\r\nNew /myWords ?"];
+            $messages[] = ['method' => 'sendSticker', 'content' => 'sticker', 'value' => 'CAACAgIAAxkBAAEZ0fVjbQMjLMWdsAaB-RKhE_ZqUKzRpwACUgEAAjDUnRERwgZS_w81pCsE'];
+            $messages[] = ['method' => 'sendMessage', 'content' => 'text', 'value' => "Yeap! It is $cache->rightAnswer!\r\nYou scored 5 points\r\Continue /myWords ?"];
 
             User::where('id', $this->id)->increment('points', 5);
             DB::table($this->id."_vocabulary")->where('word_id', $cache->rightAnswerId)->increment('points', 5);
           } else {
-            $messages[] = ['method' => 'sendSticker', 'content' => 'sticker', 'value' => 'CAACAgIAAxkBAAEVJTJirgrIonNCuE8zH9G922WV4e4tmQACDQADwDZPE6T54fTUeI1TJAQ'];
-            $messages[] = ['method' => 'sendMessage', 'content' => 'text', 'value' => "Nope. You've lost a point\r\nRight answer was $cache->rightAnswer!\r\nNew /myWords ?"];
+            $messages[] = ['method' => 'sendSticker', 'content' => 'sticker', 'value' => 'CAACAgIAAxkBAAEZ0fdjbQM6T1lg7zkTF7n251_knccYbgACNwEAAjDUnRHKK3SQd2L8ASsE'];
+            $messages[] = ['method' => 'sendMessage', 'content' => 'text', 'value' => "Nope. You've lost a point\r\nRight answer was $cache->rightAnswer!\r\Continue /myWords ?"];
 
             User::where('id', $this->id)->decrement('points', 1);
             DB::table($this->id."_vocabulary")->where('word_id', $cache->rightAnswerId)->decrement('points', 1);
