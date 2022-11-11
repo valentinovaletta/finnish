a3230adb45f053c042ef8ca1b81a4a80dff88a35
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2022-11-11 11:39:36 +0200

    new word set

--- app/Lib/Message/Messages/MessageNewWords.php
+++ app/Lib/Message/Messages/MessageNewWords.php
@@ -24,8 +24,8 @@ class MessageNewWords extends Message {
         $wordSets = Tag::orderBy('id')->get();
 
         $text = "There are new such Word Sets as\r\n";
-        foreach($wordSets as $key => $set){
-            $text .= $key.") ". $set->tag_name."\r\n";
+        foreach($wordSets as $set){
+            $text .= "/".$set->id.") ". $set->tag_name."\r\n";
         }
         $text .= "Click on Word Set Id to subscribe on it.";
 
