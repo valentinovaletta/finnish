beba0e05f6feb32ae351cc6f55e0b2d6fedf83c9
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2022-11-11 10:56:07 +0200

    check img

--- app/Lib/Newwords/CambridgeParserLibrary.php
+++ app/Lib/Newwords/CambridgeParserLibrary.php
@@ -24,8 +24,8 @@ class CambridgeParserLibrary {
     public function __construct() {
         //$this->word = $this->getWordFromDB(1); // regular word
         $this->word = $this->getWordFromDBwithoutImg(1); // without image
-        //$this->img = $this->GetImgUnsplashApi( $this->word ); // substr($this->word, 2)
-        $this->img = $this->GetImgpixabayApi(urlencode(trim($this->word)));
+        $this->img = $this->GetImgUnsplashApi( $this->word ); // substr($this->word, 2)
+        //$this->img = $this->GetImgpixabayApi(urlencode(trim($this->word)));
         //$this->CambridgeObj = $this->getYandexApiDictionary( $this->word ); // substr($this->word, 2)
     }
 
@@ -97,9 +97,9 @@ class CambridgeParserLibrary {
     }
 
     private function GetImgUnsplashApi($word){
-        $UnsplashResponce = $this->CallDictionaryApi("https://api.unsplash.com/search/photos/?client_id=3d5fKAxk_gmo9I8XI20kCQWf0j0r1foLd6E7kuLaq0k&page=1&per_page=1&query=".urlencode(trim($word)));
+        $UnsplashResponce = $this->CallDictionaryApi("https://api.unsplash.com/search/photos/?client_id=3d5fKAxk_gmo9I8XI20kCQWf0j0r1foLd6E7kuLaq0k&page=1&per_page=1&query=".$word);
 
-        print_r("https://api.unsplash.com/search/photos/?client_id=3d5fKAxk_gmo9I8XI20kCQWf0j0r1foLd6E7kuLaq0k&page=1&per_page=1&query=".urlencode(trim($word)));
+        print_r("https://api.unsplash.com/search/photos/?client_id=3d5fKAxk_gmo9I8XI20kCQWf0j0r1foLd6E7kuLaq0k&page=1&per_page=1&query=".$word);
 
         if( array_key_exists('results', $UnsplashResponce) && $UnsplashResponce['total'] > 0 ){
             $img = $UnsplashResponce['results'][0]['urls']['small'];
