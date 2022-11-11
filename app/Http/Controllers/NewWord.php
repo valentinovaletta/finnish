d42068b584be8df1a32b1071ffabcf2f31084b04
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2022-11-11 11:10:18 +0200

    check img

--- app/Http/Controllers/NewWord.php
+++ app/Http/Controllers/NewWord.php
@@ -9,6 +9,7 @@ class NewWord extends Controller{
     public function index(){
         $newCambridgeWord = NEW CambridgeParserLibrary();
         print_r($newCambridgeWord->getNewWordObj());
+        return $newCambridgeWord->updateimg();
         //return $newCambridgeWord->saveNewWordInDb();
         //return $newCambridgeWord->getNewWordObj();
     }
