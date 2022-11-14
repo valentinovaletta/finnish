56717e74c76fbb535a52d1a6780de599545c32c0
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2022-11-14 11:30:20 +0200

    copy new words

--- app/Lib/Message/Messages/MessageDefault.php
+++ app/Lib/Message/Messages/MessageDefault.php
@@ -83,8 +83,8 @@ class MessageDefault extends Message {
 
     private function CopyNewWords($newWordSet){
         if( $newWordSet ){
-            $wordIds = TagWord::select('word_id', '0 as points')->where('tag_id', $this->param['command'])->toSQL();
-            print_r($wordIds, true);
+            $wordIds = TagWord::select('word_id')->where('tag_id', $this->param['command'])->get();
+            //print_r($wordIds, true);
             // $upsert = DB::table($this->id."_vocabulary")->upsert([
             //     ['word_id' => 401, 'points' => 0],
             //     ['word_id' => 402, 'points' => 0]
