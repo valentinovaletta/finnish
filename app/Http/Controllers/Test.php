b4b6ec87e0e6b7b0fba3e3e9176e68cfe08a61cc
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2022-11-14 11:38:54 +0200

    copy new words

--- app/Http/Controllers/Test.php
+++ app/Http/Controllers/Test.php
@@ -9,10 +9,14 @@ class Test extends Controller{
     private $id;
 
     public function index(){
-
+        echo '<h1>TEST</h1>';
         $this->id = 494963311;
 
-        $wordIds = TagWord::where('tag_id', 5)->toSQL();
+        //$wordIds = TagWord::where('tag_id', 5)->toSQL();
+        $wordIds = TagWord::select('id', 'tag_id', 'word_id')
+        ->where('tag_id', '=', '5')
+        ->get();
+
         print_r($wordIds, true);
 
     }
