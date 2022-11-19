e66428f002562f8839b4e75a6b3618af4c2d13c0
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2022-11-19 19:37:50 +0200

    token

--- resources/lang/en/telegram.php
+++ resources/lang/en/telegram.php
@@ -2,6 +2,6 @@
 
 return [
 
-    'info' => "Hello!\r\nThis is an Info message!\r\nYour id is :id\r\nYour name is :name\r\nYour lang is :lang",
+    'info' => "Hello!\r\nThis is an Info message!\r\nIt is from lang folder\r\nYour id is :id\r\nYour name is :name\r\nYour lang is :lang",
 
 ];
