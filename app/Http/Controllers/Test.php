1ff08c451832c4b82801a1d6b8426d7f6d529f8d
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2022-12-03 11:02:07 +0200

    to sql

--- app/Http/Controllers/Test.php
+++ app/Http/Controllers/Test.php
@@ -24,7 +24,7 @@ class Test extends Controller{
           ->whereNull('tag_users.tag_id')
           ->inRandomOrder()
           ->take(5)
-          ->toSQL();
+          ->get();
 
         print_r($sets);  
 // Select      t1.*
