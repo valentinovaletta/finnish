9a3537b7ff9d20a96afb5bb8acf12354446e121a
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2022-12-04 10:45:32 +0200

    example

--- app/Lib/CreateImage/CreateImage.php
+++ app/Lib/CreateImage/CreateImage.php
@@ -8,9 +8,11 @@ class CreateImage {
 
     private $id;
     private $word;
+    private $ex;
 
     public function __construct() {
         $this->word = $this->getWordFromDB();
+        $this->ex = $this->getYandexApiDictionary($this->word);
     }
 
     private function getWordFromDB(){
@@ -22,10 +24,26 @@ class CreateImage {
     public function show(){
         $data = [
             'id' => $this->id,
-            'word' => $this->word
+            'word' => $this->word,
+            'ex' => $this->ex
         ];
 
         return $data;
     }
 
+    private function getYandexApiDictionary($word){
+        $dictionaryJson = $this->CallDictionaryApi("https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20220605T165718Z.d29bca9ff5cc7d61.ae331150aaba52f73b5ad4d8bce3564ea9028917&lang=en-en&text=".urlencode(trim($word)));
+        return isset($dictionaryJson['def'][0]['tr'][0]['ex'][0]['text']) ? $dictionaryJson['def'][0]['tr'][0]['ex'][0]['text'] : '';
+    }
+
+    private function CallDictionaryApi($url){
+
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        $resp = curl_exec($curl);
+        curl_close($curl);
+        return json_decode($resp, true);
+    }
 }
