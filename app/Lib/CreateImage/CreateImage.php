98f62a399e87cdad5f8a2e3fa95aad770e5e17cc
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2022-12-06 16:20:20 +0200

    image

--- app/Lib/CreateImage/CreateImage.php
+++ app/Lib/CreateImage/CreateImage.php
@@ -44,12 +44,12 @@ class CreateImage {
         }
         $img = Image::make($imgUrl);
 
-        $img->rectangle(0, 735, 800, 800, function ($draw) {
+        $img->rectangle(0, 0, 800, 65, function ($draw) {
             $draw->background('#000');
         });
 
-        $img->text($word." - ".$ruWord, 10, 785, function($font) {
-            $font->file(public_path('fonts/'.rand(1,4).'.ttf'));
+        $img->text($word." - ".$ruWord, 10, 10, function($font) {
+            $font->file(public_path('fonts/'.rand(2,4).'.ttf'));
             $font->size(36);
             $font->color( '#fff' );
             $font->align('left');
