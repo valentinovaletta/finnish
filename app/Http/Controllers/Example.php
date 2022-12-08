8ea441bec0c7bdc0e004f5891bf86198974fe85a
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2022-12-08 14:31:56 +0200

    vk post

--- app/Http/Controllers/Example.php
+++ app/Http/Controllers/Example.php
@@ -19,6 +19,7 @@ class Example extends Controller{
         $token = "vk1.a.CkSpYnY7gNEBVKDfvfyyAWQGA3k6r8tzr7RFEHq4pWj5OLg7L1_9MmblaZxnGhEk0YXc-Ak7gn_7pR3fnQs7Ke3CnKyu6vqkBKV3Ku9KadsWrgBmr76j_7YgQdwQYlW99QypylCQ4-9HrIpeBX_24riP6pqRmbdc7EKLpjw0RcIUFbWuo2xRiCY_wLRVVQuQK6q39XqMfBUBu5FF-8yPvg";
         $group_id = '212716589';
         $i = 0;
+        $attachments='';
         $vk = new Vk($token);
         
         $imgs = [
@@ -29,20 +30,21 @@ class Example extends Controller{
         $upload_server = $vk->photosGetWallUploadServer($group_id);
 
         foreach($imgs as $img){
-            $upload[$i++] = $vk->uploadFile($upload_server['upload_url'], $img);
+            $uploads[$i++] = $vk->uploadFile($upload_server['upload_url'], $img);
         }
 
-        $save = $vk->photosSaveWallPhoto([
+        foreach($uploads as $upload){
+            $saves[$i++] = $vk->photosSaveWallPhoto([
                 'group_id' => $group_id,
-                'photo' => $upload[0]['photo'],
-                'server' => $upload[0]['server'],
-                'hash' => $upload[0]['hash']
-            ]
-        );
+                'photo' => $upload['photo'],
+                'server' => $upload['server'],
+                'hash' => $upload['hash']
+            ]);        
+        }
 
-        var_dump($save);
-
-        $attachments = sprintf('photo%s_%s', $save[0]['owner_id'], $save[0]['id']);
+        foreach($saves as $save){
+            $attachments .= sprintf('photo%s_%s', $save['owner_id'], $save['id']);
+        }
         
         $post = $vk->wallPost([
             'owner_id' => "-$group_id",
