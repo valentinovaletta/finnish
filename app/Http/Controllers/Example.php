0fe0e595b0ed9906e3ddc8978c121ea1e7e24954
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2022-12-08 13:42:49 +0200

    vk post

--- app/Http/Controllers/Example.php
+++ app/Http/Controllers/Example.php
@@ -16,7 +16,7 @@ class Example extends Controller{
 
     public function vk(){
 
-        $token = "vk1.a.SK_FTVNVE_hYo3NZbS_Q28pdU2E88rZqfW-bpjYUnYckdxaqyspZ8-u3SgoupyRzcGWIX8LEqw_9K7ix-BPPtdp4D6bfChElzHrG-_cDQOCf0wwk-NA-gitku5T3IBR8KJsJ9Qo2VjMmRhWTvi6BH5K54nWIkwcMCsje1yoAqljT8EoJovVfHrucG2wRK2WU2wGXz-UIxhgAHzyHIInEwg";
+        $token = "vk1.a.CkSpYnY7gNEBVKDfvfyyAWQGA3k6r8tzr7RFEHq4pWj5OLg7L1_9MmblaZxnGhEk0YXc-Ak7gn_7pR3fnQs7Ke3CnKyu6vqkBKV3Ku9KadsWrgBmr76j_7YgQdwQYlW99QypylCQ4-9HrIpeBX_24riP6pqRmbdc7EKLpjw0RcIUFbWuo2xRiCY_wLRVVQuQK6q39XqMfBUBu5FF-8yPvg";
         $group_id = '-212716589';
         $vk = new Vk($token);
         
