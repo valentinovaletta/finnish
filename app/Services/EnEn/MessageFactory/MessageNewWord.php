b072586da14d33461e13021b9fedb13da745fbb8
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2023-03-29 14:57:16 +0300

    method fix

---
+++ app/Services/EnEn/MessageFactory/MessageNewWord.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Services\EnEn\MessageFactory;
+
+use Illuminate\Support\Facades\DB;
+use App\Models\Cache;
+
+class MessageNewWord extends Message{
+
+    private $chatId;
+    private $param;
+
+    private $quizFunctions = [0 => 'DefenitionToWord', 1 => 'DefenitionToWord'];
+
+    public function __construct(int $chatId, array $param){
+        $this->chatId = $chatId;
+        $this->param = $param;
+
+        $words = $this->getWords();
+        $quizfunc = $this->quizFunctions[rand(0, count($this->quizFunctions)-1 )];
+
+        $this->setKeyboard(json_encode(["inline_keyboard" => [[["text" => "Назад","callback_data" => "StartAgain/"]]]]));
+        $this->setMessage(['method' => 'editMessageText', 'param' => ['chat_id' => $this->chatId, 'message_id' => $this->param['message_id'], 'text' => print_r($quizfunc, true), 'reply_markup'=>$this->keyboard]]);
+    }
+
+    private function getWords(){
+        $words = DB::table($this->chatId.'_vocabulary_enen')
+        ->join('en_dictionaries', $this->chatId.'_vocabulary_enen.word_id', '=', 'en_dictionaries.id')
+        ->join('fi_dictionaries', $this->chatId.'_vocabulary_enen.word_id', '=', 'fi_dictionaries.id')
+        ->select($this->chatId.'_vocabulary_enen.word_id as id', $this->chatId.'_vocabulary_enen.points as points', 'en_dictionaries.word as enword', 'en_dictionaries.pos as pos', 'en_dictionaries.ts as ts', 'en_dictionaries.img as img', 'fi_dictionaries.word as fiword')
+        ->orderBy($this->chatId.'_vocabulary_enen.points')
+        ->inRandomOrder()
+        ->take(4)
+        ->get();
+        return $words;
+    }
+
+    private function DefenitionToWord($words){
+        // choice right answer 
+        $rightAnswerId = $words->first()->id;
+        $rightAnsweren = $words->first()->enword;
+        $rightAnswerfi = $words->first()->fiword;
+        $rightAnswerPos = $words->first()->pos;
+        $rightAnswerTs = $words->first()->ts;
+        $rightAnswerImg = $words->first()->img;
+
+        $answers = [
+            $words->get(0)->fiword,
+            $words->get(1)->fiword,
+            $words->get(2)->fiword,
+            $words->get(3)->fiword
+        ];
+
+        shuffle($answers);
+  
+        // form a question and answers
+        $text = $rightAnsweren."\r\n(".$rightAnswerPos.") [".$rightAnswerTs."] \r\n";
+        $text .= __('telegram.myWordsWhatisitEnFi');
+
+        return json_encode([
+            'question' => $text,
+            'answers' => $answers
+        ]);    
+    }
+
+}
