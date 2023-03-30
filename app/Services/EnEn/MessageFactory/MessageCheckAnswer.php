d30331e8f07473e5cece495748b6bc66b5dc7c08
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2023-03-30 12:35:26 +0300

    cache

--- app/Services/EnEn/MessageFactory/MessageCheckAnswer.php
+++ app/Services/EnEn/MessageFactory/MessageCheckAnswer.php
@@ -17,18 +17,7 @@ class MessageCheckAnswer extends Message {
         $this->param = $param;
         
         $check = $this->checkAnswer($this->param['commandArg']);
-        //$text = print_r($check, true);
-        if(!$check['status']){
-            $text = __('telegram.IncorrectAnswer', ['answer' => $check['rightAnswerText']]);
-            User::where('id', $this->chatId)->decrement('points', 1);
-            DB::table($this->chatId."_vocabulary_enen")->where('word_id', $check['rightAnswer'])->decrement('points', 1); 
-
-        } else {
-            $text = __('telegram.CorrectAnswer', ['answer' => $check['rightAnswerText']]);
-
-            User::where('id', $this->chatId)->increment('points', 3);
-            DB::table($this->chatId."_vocabulary_enen")->where('word_id', $check['rightAnswer'])->increment('points', 3);           
-        }
+        $text = $this->formResponce($check);
 
         $this->setKeyboard(json_encode(["inline_keyboard" => [[["text" => __('keyboard.GoOn'), "callback_data" => "NewWord/"]]]]));
         $this->setMessage(['method' => 'editMessageText', 'param' => ['chat_id' => $this->chatId, 'message_id' => $this->param['message_id'], 'text' => $text, 'reply_markup'=>$this->keyboard]]);
@@ -37,6 +26,19 @@ class MessageCheckAnswer extends Message {
     private function checkAnswer($userAnswer){
         $cache = json_decode( Cache::get($this->chatId), true );
         return ['status' => ($cache['id'] == $userAnswer), 'rightAnswer' => $cache['id'], 'rightAnswerText' => $cache['answer'], 'userAnswer' => $userAnswer];
+    }
+
+    private function formResponce($check){
+
+        if(!$check['status']){
+            User::where('id', $this->chatId)->decrement('points', 1);
+            DB::table($this->chatId."_vocabulary_enen")->where('word_id', $check['rightAnswer'])->decrement('points', 1); 
+            return __('telegram.IncorrectAnswer', ['answer' => $check['rightAnswerText']]);
+        }
+
+        User::where('id', $this->chatId)->increment('points', 3);
+        DB::table($this->chatId."_vocabulary_enen")->where('word_id', $check['rightAnswer'])->increment('points', 3);  
+        return __('telegram.CorrectAnswer', ['answer' => $check['rightAnswerText']]);
     }
 
 }
