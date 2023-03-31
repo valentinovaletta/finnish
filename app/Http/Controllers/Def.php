d9c8be9bd13a1a7077953409320a897b34b04266
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2023-03-31 14:34:45 +0300

    dictionary api

--- app/Http/Controllers/Def.php
+++ app/Http/Controllers/Def.php
@@ -10,7 +10,7 @@ class Def extends Controller{
     public function index(){
         
         $word = EnDictionary::where('ex', '')->first();
-        print_r($word->get(0)->word);
+        print_r($word->first()->word);
     }
 
 }
